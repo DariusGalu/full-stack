814ef0701500693f201aa8db6d698d54f478534f
DariusGalu <galu.darius77@gmail.com>
2020-12-21 14:06:38 +0200

    init

--- src/form.js
+++ src/form.js
@@ -3,37 +3,22 @@ import React, { useEffect, useState } from 'react';
 function Form() {
 
     const [email, setEmail] = useState([]);
-    const [dateofbirth, setDateofBirth] = useState('');
     const [newEmail, setNewEmail] = useState('');
-    const [newDateofbirth, setNewDateofbirth] = useState('');
 
 
     useEffect(() => {
         const readEmail = () => {
-            if(localStorage.getItem('email')) {
+            if(localStorage.getItem('email'))  {
                 setEmail(JSON.parse(localStorage.getItem('email')));
-                
             }
         }
         readEmail()
     }, []);
 
-    useEffect(() => {
-        const readBirth = () => {
-            if(localStorage.getItem('birth')) {
-                setDateofBirth(JSON.parse(localStorage).getItem('birth'))
-            }
-        }
-        readBirth()
-    }, []);
-
     const onCreate = () => {
         email.push(newEmail)
-        dateofbirth.push(newDateofbirth)
         localStorage.setItem('email', JSON.stringify(email))
-        localStorage.setItem('birth', JSON.stringify(dateofbirth))
         setNewEmail('');
-        setNewDateofbirth('');
     };
     return (
         <div className="form">
@@ -43,8 +28,8 @@ function Form() {
                     <h5>Email</h5>
                     <input value={newEmail} onChange={e => setNewEmail(e.target.value)} type="email"/>
                     <h5>Date of Birth</h5>
-                    <input value={newDateofbirth} onChange={e => setNewDateofbirth(e.target.value)} type="dateofbirth"/>
-                    <button variant="primary" onClick={onCreate}>Submit</button>
+                    {/* <input value={newDateofbirth} onChange={e => setNewDateofbirth(e.target.value)} type="date"/> */}
+                    <button onClick={onCreate}>Submit</button>
                 </form>
             </div>
         </div>
